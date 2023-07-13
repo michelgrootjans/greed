d1fc230b471fabcd6b55574a6632a265db53e0ab
Michel Grootjans <michel.grootjans@gmail.com>
2023-07-13 13:52:31 +0200

    basic socket template

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,7 +1,33 @@
-export default function Home() {
+'use client'
+
+import React, {useEffect} from "react";
+
+let socket;
+
+const Home = () => {
+    useEffect(() => {
+        console.log('useEffect')
+        socketInitializer();
+
+        return () => {
+            console.log('socket.disconnect()')
+            socket.disconnect();
+        };
+    }, []);
+
+    async function socketInitializer() {
+        console.log('socketInitializer')
+    }
+
+    function handleSubmit(e) {
+        console.log('handleSubmit')
+    }
+
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            chat
-        </main>
-    )
-}
+        <div>
+            <h1>Chat app</h1>
+        </div>
+    );
+};
+
+export default Home;
