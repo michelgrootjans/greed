b7f1c67a7c22f946e5a40c697c1cd263335457c0
Michel Grootjans <michel.grootjans@gmail.com>
2023-07-14 13:17:29 +0200

    cleanup

--- src/app/api/games/[gameId]/route.ts
+++ src/app/api/games/[gameId]/route.ts
@@ -1,5 +1,4 @@
 import {NextRequest, NextResponse} from 'next/server'
-import {param} from "ts-interface-checker";
 
 export async function GET(request: NextRequest, {params}) {
     console.log({ request, params })
