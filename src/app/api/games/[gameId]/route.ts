7e28a958c2cbd307de18ba69ae9c80cfac5d124d
Michel Grootjans <michel.grootjans@gmail.com>
2023-07-19 09:43:35 +0200

    cleanup

--- src/app/api/games/[gameId]/route.ts
+++ src/app/api/games/[gameId]/route.ts
@@ -4,7 +4,7 @@ import {gameCount} from "@/lib/games";
 export async function GET(request: NextRequest, {params}) {
     const games = gameCount();
 
-    console.log({ request, params: params, games })
-    return NextResponse.json({ request, params: params, games });
+    console.log("POST /games/{id}", { params: params, games })
+    return NextResponse.json({ params: params, games });
 }
 
--- src/app/api/games/route.ts
+++ src/app/api/games/route.ts
@@ -2,21 +2,17 @@ import {NextRequest, NextResponse} from 'next/server'
 import {createGame, gameCount} from "@/lib/games";
 
 export async function GET(request: NextRequest, {params}: {params: {slug: string}}) {
-    // if (params && params.slug) {
-    //     const slug = params.slug
-    // }
-
     const games = gameCount();
 
-    console.log({ request, params: params, games })
-    return NextResponse.json({ request, params: params, games });
+    console.log("GET /games", { params: params, games })
+    return NextResponse.json({ params: params, games });
 }
 
 export async function POST(request: NextRequest, {params}: {params: {slug: string}}) {
     const games = gameCount();
     createGame("5")
 
-    console.log({ request, params: params, games })
+    console.log("POST /games", { params: params, games })
     return NextResponse.json({});
 }
 
